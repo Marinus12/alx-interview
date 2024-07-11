d1e8d12e03860d2ba717917c6444529f77f05ebf
Marinus Ukewuihe <dubemarinus@gmail.com>
2024-07-11 23:04:03 +0000

    0x02. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    # Find all factors of n
+    while n > 1:
+        while n % divisor == 0:
+            # Add the divisor to operations count
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
