58a41b1e12a5fbab52d9b472b311088447b6a3bb
Marinus Ukewuihe <dubemarinus@gmail.com>
2024-08-08 21:40:25 +0000

    New commits

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,19 @@
 #!/usr/bin/node
-const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
+const request = require('request');
 
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-
-  characters.forEach(character => {
-    request(character, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error(charError);
-        return;
-      }
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     });
-  });
+  }
 });
