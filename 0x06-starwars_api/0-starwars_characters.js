15264f2104328e1c0d2af2412908cd731c1bc3b9
Marinus Ukewuihe <dubemarinus@gmail.com>
2024-08-11 21:44:06 +0000

    0x06 Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,15 +5,28 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(url, function (error, response, body) {
+function getCharacterName(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(JSON.parse(body).name);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+request(url, async function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    for (const character of characters) {
+      try {
+        const name = await getCharacterName(character);
+        console.log(name);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 });
