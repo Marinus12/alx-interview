b7ba27268ee9ded54df84ec3e9f1f60e63ada133
Marinus Ukewuihe <dubemarinus@gmail.com>
2024-08-15 09:10:06 +0000

    0x07. Rotate 2D Matrix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
- """
+
+
+def pascal_triangle(n):
+    """
     Returns a list of lists of integers representing the Pascal’s triangle of n.
     Returns an empty list if n <= 0.
     """
-    
-
-def pascal_triangle(n):
     if n <= 0:
         return []
 
--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,10 +3,10 @@
 Rotates a 2D matrix 90 degrees clockwise.
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise in-place.
-    
     Args:
         matrix (list of list of int): A 2D matrix to rotate.
     """
