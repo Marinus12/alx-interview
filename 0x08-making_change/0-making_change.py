69e3051424aadb44dbeff364be828fe1f461f4e5
Marinus Ukewuihe <dubemarinus@gmail.com>
2024-08-25 15:54:42 +0000

    0x08. Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,6 +16,6 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
     for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[total] if dp[total] != float('inf') else -1
